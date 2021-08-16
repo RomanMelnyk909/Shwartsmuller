6faa643b23a7e3d79bb2128a79f6583af148bbe2
RomanMelnyk909 <45914004+RomanMelnyk909@users.noreply.github.com>
2021-08-16 21:35:40 +0300

    add components with props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-  
+
+import Expenses from './components/Expenses/Expenses'
+
 function App() {
+  let expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "Paper",
+      amount: 29.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e3",
+      title: "Anoter costs",
+      amount: 2.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Last expense",
+      amount: 67,
+      date: new Date(2021, 2, 28),
+    },
+  ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Lets started</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
